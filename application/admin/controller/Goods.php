2e34aaa5121b8d9952a994974f6f13903b86e4c8
mitu1995 <1152155861@qq.com>
2020-02-22 16:30:15 +0800

    完成商品修改

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -18,13 +18,28 @@ class Goods extends BaseController
         if (request()->isAjax()){
           $model = new  goodsModel();
           $model ->add();
-
         }
         $cate = Db::table('category')->field('id,name')->select();
         $this->assign('cate',$cate);
         return $this->fetch();
     }
+    //商品编辑
+    public function edit()
+    {
+        $model = new  goodsModel();
+        $goods =  $model->getGoodsOne(input('get.id'));
+        if (request()->isAjax()){
+            $model->edit();
+        }else{
+            $cate = Db::table('category')->field('id,name')->select();
+            $this->assign('cate',$cate);
+            $this->assign('goods',$goods);
+            return $this->fetch();
+        }
 
+
+    }
+    //商品删除
     public function goods_del()
     {
         (new  goodsModel())->del();
--- application/admin/model/Goods.php
+++ application/admin/model/Goods.php
@@ -39,7 +39,7 @@ class Goods extends BaseModel
 
             throw new ParamException([
                 'code' => 401,
-                'msg' => '角色添加失败',
+                'msg' => '商品添加失败',
                 'errorCode' => 1
             ]);
         }
@@ -47,6 +47,40 @@ class Goods extends BaseModel
         public function goodsList()
         {
             return self::with(['imgs','cates'])->order('order','asc')->order('id desc')->select();
+        }
+        //获取一条商品信息
+        public function getGoodsOne($id)
+        {
+            return self::with(['imgs','cates'])->where('id',$id)->find();
+
+        }
+        //编辑商品
+        public function edit()
+        {
+            $data = (request()->except(['file']));
+            if (! is_numeric($data['img'])){
+                //删除原图
+                $path = Db::table('img')->field('img')->find(['id'=>$data['img_id']]);
+                delImg($path['img']);
+                //获取上传的图片ID
+                $img_id = Db::table('img')->insertGetId(['img'=>$data['img']]);
+                $data['img'] = $img_id;
+            }
+            unset($data['img_id']);
+            if (self::update($data)){
+                throw new ParamException([
+                    'code'=>'200',
+                    'msg'=>'商品更新成功',
+                    'errorCode'=>0,
+                ]);
+            }
+            throw new ParamException([
+                'code'=>'400',
+                'msg'=>'商品更新失败',
+                'errorCode'=>1,
+            ]);
+
+
         }
         //删除商品
         public function del(){
